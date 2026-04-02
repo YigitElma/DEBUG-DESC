fe5dc0071e86f7b5dad24290c955f9d6b66ed5c9
YigitElma <yigitelmacioglu@gmail.com>
2026-04-01 23:00:22 -0400

    add couple more scripts for debugging

--- HBT-EP/convert-eqdsk2desc.py
+++ HBT-EP/convert-eqdsk2desc.py
@@ -1,7 +1,13 @@
 import sys
+import os
 
 # this should be where you cloned the efit2desc repo
 sys.path.insert(1, "../../../efit2desc")
+sys.path.append(os.path.abspath("../../"))
+
+# from desc import set_device
+
+# set_device("gpu")
 
 # to make things work properly use a new environment
 # for efit2desc
@@ -10,15 +16,43 @@ from efit2desc import (
     plot_eq_iota_against_efit,
     plot_eq_surfaces_against_efit,
 )
+import numpy as np
 
-name = "HBT_105995_06"
-eqdsk_name = f"From-Jeff/{name}.eqdsk"
+name = "hbt_free_boundary2"
+eqdsk_name = f"tokamaker/{name}.eqdsk"
 
 # I had to comment some saving functions in the source code
 # if you give a relative path, internal file names becomes
 # weird
-eq, _ = convert_EFIT_to_DESC(
-    eqdsk_name, L=16, M=16, psiN_cutoff=1.0, plot=False, save=False
-)
-eq.change_resolution(NFP=64)
-eq.save(f"desc-eq-{name}.h5")
+# eq, _ = convert_EFIT_to_DESC(
+#     eqdsk_name,
+#     L=24,
+#     M=24,
+#     psiN_cutoff=1.0,
+#     plot=False,
+#     save=False,
+#     solve=False,
+# )
+# eq.change_resolution(NFP=64)
+# eq.save(f"desc-eq-{name}-3.h5")
+
+from omfit_classes import omfit_eqdsk
+
+efit = omfit_eqdsk.OMFITgeqdsk(eqdsk_name)
+# populate aux and flux-surface quantities
+efit.addAuxQuantities()
+efit.addFluxSurfaces(levels=list(np.linspace(0, 1, 129)))
+# surfAvg computes safety factor, pressure, etc. on each flux surface
+efit["fluxSurfaces"].surfAvg()
+
+
+def list_keys(data, lvl=1):
+    for key, value in data.items():
+        if isinstance(value, dict):
+            list_keys(value, lvl=lvl + 1)
+        else:
+            arrow = "--" * lvl
+            print(f"{arrow}>{key}")
+
+
+list_keys(efit, lvl=1)
